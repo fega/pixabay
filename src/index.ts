af9b3f479f962928be71128e5721cca9d5eb3e28
Fabian Gutierrez <fega.hg@gmail.com>
2019-02-14 09:44:51 -0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export * from './lib/number';
+/**
+ * https://pixabay.com/api/docs/
+ */
+
+import axios from 'axios';
+import qs from 'qs';
+
+
+export class Pixabay {
+  private URL = 'https://pixabay.com/api/'
+  constructor(private readonly apiKey) {
+  }
+
+  async get(query, options) {
+    const ops = qs.stringify({
+      q: query,
+      image_type: 'photo',
+      webformatURL: '_960',
+      key: this.apiKey,
+      ...options,
+    });
+    const url = `${this.URL}?${ops}`;
+    const { data } = await axios.get(url);
+
+    return data;
+  }
+}
